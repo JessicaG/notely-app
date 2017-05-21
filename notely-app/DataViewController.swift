848de017befbd5c6e7a52bd9dde0e96b7b047912
Jessica West <jessicawest@Jessicas-MacBook.local>
2017-05-21 16:08:25 -0600

    Initial Commit

---
+++ notely-app/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  notely-app
+//
+//  Created by Jessica West on 5/21/17.
+//  Copyright © 2017 Jessica West. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
